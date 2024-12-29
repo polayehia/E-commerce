c96effa84744336ceccb67c1133694ed25f14e5e
polayehia <124912643+polayehia@users.noreply.github.com>
2024-12-29 23:30:05 +0200

    Update Wishlist.jsx

--- src/componant/Wishlist.jsx
+++ src/componant/Wishlist.jsx
@@ -26,7 +26,7 @@ export default function Wishlist() {
       return <Loading></Loading>
       if (isError)
 return <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-      <span className="font-medium">{error}</span> 
+      <span className="font-medium">{error?.message||'An error occurred'}</span> 
     </div>
       
 if(status==='success')
